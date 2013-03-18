50dccee84e51ac112fdef184ac67fc6d8e0b1019
kadirpekel <kadirpekel@gmail.com>
2013-03-18 12:41:12 +0200

    refactored tests and used mock lib when required

--- tests.py
+++ tests.py
@@ -9,18 +9,21 @@ from simmetrica import Simmetrica
 
 class TestSimmetrica(unittest.TestCase):
 
-    def setUp(self):
-        self.simmetrica = Simmetrica()
-
     def test_push(self):
-        self.assertEqual(8, len(self.simmetrica.push('foo')))
+        with mock.patch('simmetrica.StrictRedis') as StrictRedis:
+            simmetrica = Simmetrica()
+            hincrby = StrictRedis.return_value.pipeline.return_value.hincrby
+            simmetrica.push('foo')
+            self.assertTrue(hincrby.called)
 
     def test_round_time(self):
-        rounded_time = self.simmetrica.round_time(1363599249, 3600)
+        simmetrica = Simmetrica()
+        rounded_time = simmetrica.round_time(1363599249, 3600)
         self.assertEqual(rounded_time, 1363597200)
 
     def test_get_event_key(self):
-        key = self.simmetrica.get_event_key('foo', '5min')
+        simmetrica = Simmetrica()
+        key = simmetrica.get_event_key('foo', '5min')
         self.assertEqual('simmetrica:foo:5min', key)
 
 if __name__ == '__main__':
