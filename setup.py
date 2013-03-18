c37038e8449c53e1c99907287c4d56e30ddd8df6
Osman Ungur <osmanungur@gmail.com>
2013-03-18 11:51:46 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='simmetrica',
+    packages=['simmetrica'],
+    version='0.0.1',
+    description='Library for collecting, aggregating and visualizing event metrics as timeseries data',
+    author='Osman Ungur',
+    author_email='osmanungur@gmail.com',
+    url='https://github.com/import/simmetrica'
+)
