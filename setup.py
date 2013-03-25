feccd084182f7dceb733891276adedd51d7ab8f4
Osman Ungur <osmanungur@gmail.com>
2013-03-25 10:46:09 +0200

    Versioning, install_requires and syntax fix

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ from setuptools import setup
 setup(
     name='simmetrica',
     py_modules=['simmetrica'],
-    version='0.0.1',
+    version='0.4.0',
     description='Library for collecting, aggregating and visualizing event '
                 'metrics as timeseries data',
     author='Osman Ungur',
     author_email='osmanungur@gmail.com',
-    url='https://github.com/import/simmetrica'
-    install_requires=[
-        'redis'
-    ],
+    url='https://github.com/import/simmetrica',
+    install_requires=['redis','flask','pyyaml'],
     tests_require=['mock'],
 )
